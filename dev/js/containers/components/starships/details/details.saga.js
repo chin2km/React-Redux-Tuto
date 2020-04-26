bff902d9fb85a87bcd46c9c89a1a7f806cf22363
Chintu Mohandas <chintu.mohandas@gmail.com>
2020-04-26 14:19:32 +0300

    updated api

--- dev/js/containers/components/starships/details/details.saga.js
+++ dev/js/containers/components/starships/details/details.saga.js
@@ -8,7 +8,7 @@ import {putFetchedData} from "./details.actions"
 export function* fetchDataByIdSaga(action) {
   try {
       yield delay(500);
-      const response = yield call(httpRequest,'GET', action.payload.url);
+      const response = yield call(httpRequest,'GET', action.payload.url.replace('http://', 'https://'));
       yield put(putFetchedData(response.data));
   } catch (error) {
   }
